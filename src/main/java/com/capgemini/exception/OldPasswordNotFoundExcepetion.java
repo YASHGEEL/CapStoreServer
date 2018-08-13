f774c16d4b37306da02f549b172af1a58aca420f
purpatil <purpatil@PUNHWD11640.corp.capgemini.com>
2018-08-13 16:55:48 +0530

    Interface for changePassword and exception

---
+++ src/main/java/com/capgemini/exception/OldPasswordNotFoundExcepetion.java
@@ -0,0 +1,31 @@
+package com.capgemini.exception;
+
+public class OldPasswordNotFoundExcepetion extends Exception{
+
+	public OldPasswordNotFoundExcepetion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OldPasswordNotFoundExcepetion(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OldPasswordNotFoundExcepetion(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OldPasswordNotFoundExcepetion(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OldPasswordNotFoundExcepetion(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
