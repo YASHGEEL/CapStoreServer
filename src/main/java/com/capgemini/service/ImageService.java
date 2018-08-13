06baaea2894f2adbc31110dfd44fafa7c81c632e
vmaremal <vmaremal@PUNHWD11656.corp.capgemini.com>
2018-08-13 14:38:34 +0530

    Added methods in image service interface

--- src/main/java/com/capgemini/service/ImageService.java
+++ src/main/java/com/capgemini/service/ImageService.java
@@ -1,5 +1,12 @@
 package com.capgemini.service;
 
+import com.capgemini.model.Admin;
+import com.capgemini.model.Merchant;
+
 public interface ImageService {
+	
+	public Merchant addImage(String imageUrl);
+	
+	public Admin approveImage(String imageUrl);
 
 }
