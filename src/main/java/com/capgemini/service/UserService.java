39cf45c01297c1f016f6ac81d355f277e0bf18cb
simullap <simullap@PUNHWD11675.corp.capgemini.com>
2018-08-13 12:21:19 +0530

    interface creation

--- src/main/java/com/capgemini/service/UserService.java
+++ src/main/java/com/capgemini/service/UserService.java
@@ -8,9 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.capgemini.model.Customer;
+import com.capgemini.model.WishList;
 import com.capgemini.repository.UserRepository;  
 @Service  
-public class UserService {
+public class UserService implements WishlistService{
+
+	@Override
+	public WishList add(WishList wishlist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WishList remove(WishList wishlist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WishList display(WishList wishlist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 
