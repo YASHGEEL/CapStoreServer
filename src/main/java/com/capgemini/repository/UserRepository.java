f3c2ac494ad4c67b95eaf97c6216bcaed17a1831
ADM-IG-HWDLAB1F <ADM-IG-HWDLAB1F@PUNHWD11652.corp.capgemini.com>
2018-08-13 15:23:13 +0530

    Search Commit

--- src/main/java/com/capgemini/repository/UserRepository.java
+++ src/main/java/com/capgemini/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.capgemini.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.capgemini.model.Customer;
+
 public interface UserRepository extends CrudRepository<Customer, Integer> {  
 }  
