4019e333533eb188c3eb9429fc547e36337c2e68
YASHGEEL <yashgeel99@gmail.com>
2018-08-13 15:08:20 +0530

    Update UserService.java

--- src/main/java/com/capgemini/service/UserService.java
+++ src/main/java/com/capgemini/service/UserService.java
@@ -11,26 +11,9 @@ import com.capgemini.model.Customer;
 import com.capgemini.model.WishList;
 import com.capgemini.repository.UserRepository;  
 @Service  
-public class UserService implements WishlistService{
+public class UserService{
 
-	@Override
-	public WishList add(WishList wishlist) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public WishList remove(WishList wishlist) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public WishList display(WishList wishlist) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	
 
 
