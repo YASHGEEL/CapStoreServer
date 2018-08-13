8410dfd54e10402321b78db918d0489cd118c71e
vumanika <vumanika@PUNHWD11680.corp.capgemini.com>
2018-08-13 13:36:52 +0530

    Added Delivery Product method in interface

---
+++ src/main/java/com/capgemini/service/DeliveryProductsService.java
@@ -0,0 +1,9 @@
+package com.capgemini.service;
+
+import com.capgemini.model.OrderDetails;
+
+public interface DeliveryProductsService {
+ 
+	public OrderDetails deliveringProducts(OrderDetails order);
+	
+}
