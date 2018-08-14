3a6f927f69779d2e5f0251c6c23af6c989ba34b2
ADM-IG-HWDLAB1F <ADM-IG-HWDLAB1F@PUNHWD11625.corp.capgemini.com>
2018-08-14 15:09:45 +0530

    Modified Service Interface PromoDisplay

--- src/main/java/com/capgemini/service/PromoDisplayService.java
+++ src/main/java/com/capgemini/service/PromoDisplayService.java
@@ -6,6 +6,6 @@ import com.capgemini.model.Product;
 import com.capgemini.model.PromoCode;
 
 public interface PromoDisplayService {
-	public abstract List<PromoCode> promoDisplay();
+	public abstract List<PromoCode> promoDisplay() ;
 	public abstract List<Product> newProductsDisplay();
 }
