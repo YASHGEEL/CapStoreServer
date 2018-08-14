413451900b6a16aa6556079eff5ad377ffc69526
ADM-IG-HWDLAB1F <ADM-IG-HWDLAB1F@PUNHWD11625.corp.capgemini.com>
2018-08-14 10:23:16 +0530

    Promo Interface Edit

--- src/main/java/com/capgemini/service/PromoService.java
+++ src/main/java/com/capgemini/service/PromoService.java
@@ -4,6 +4,6 @@ import com.capgemini.model.Product;
 import com.capgemini.model.PromoCode;
 
 public interface PromoService {
-public abstract PromoCode promoDisplay(PromoCode promocode);
+public abstract PromoCode promoDisplay(PromoCode promocode) ;
 public abstract Product newProductsDisplay(Product product);
 }
