f8a09027700878798bba694e5e35376da288cb62
Dell <Dell@NikhilThakur>
2018-08-16 01:04:26 +0530

    Commit OrderDetails Repository

---
+++ src/main/java/com/capgemini/repository/OrderDetailsRepository.java
@@ -0,0 +1,16 @@
+package com.capgemini.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.capgemini.model.OrderDetails;
+
+public interface OrderDetailsRepository extends CrudRepository<OrderDetails,Integer>{
+
+	@Query("select orderDetails from OrderDetails orderDetails where orderDetails.id=?1")
+	OrderDetails findByOrderId(int id);
+	
+	@Query("Update OrderDetails orderDetails SET orderDetails.status='RETURNED' where orderDetails.status='DELIVERED'")
+	OrderDetails updateOrderStatus();
+	
+}
