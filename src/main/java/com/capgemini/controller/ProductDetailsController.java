44cca409027bcecffda6645a57c3dce65b295575
byerra <byerra@PUNHWD11658.corp.capgemini.com>
2018-08-16 16:38:57 +0530

    comiited

---
+++ src/main/java/com/capgemini/controller/ProductDetailsController.java
@@ -0,0 +1,25 @@
+package com.capgemini.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.model.Product;
+import com.capgemini.repository.ProductDetails;
+import com.capgemini.service.ProductDetailsImpl;
+import com.capgemini.service.ProductDetailsService;
+@RestController
+public class ProductDetailsController {
+
+	@Autowired
+	ProductDetailsService service;
+	
+	@RequestMapping(value="/getProductDetails/{id}")
+	public Product getProductDetails(@PathVariable int id){
+		 return service.getProductDetails(id);
+	}
+}
