b13e9a7670ecbf35c67b22860270ba3dcb430386
sivasams <sivasams@PUNHWD11681.corp.capgemini.com>
2018-08-17 15:35:49 +0530

    Refunded functionality added

--- src/main/java/com/capgemini/repository/RefundMoneyRepository.java
+++ src/main/java/com/capgemini/repository/RefundMoneyRepository.java
@@ -2,13 +2,13 @@ package com.capgemini.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.capgemini.model.OrderDetails;
-
+@Repository
 public interface RefundMoneyRepository extends JpaRepository<OrderDetails,Integer>{
 	
 @Query(value="select o from OrderDetails o where o.id=?1")
-public OrderDetails refundDisplay(@Param("id")int id);
+public OrderDetails refundDisplay(int id);
 
 }
--- src/main/java/com/capgemini/service/RefundMoneyImpl.java
+++ src/main/java/com/capgemini/service/RefundMoneyImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.capgemini.model.Customer;
 import com.capgemini.model.OrderDetails;
+import com.capgemini.repository.OrderDetailsRepository;
 import com.capgemini.repository.RefundMoneyRepository;
 
 @Service
@@ -14,9 +15,20 @@ public class RefundMoneyImpl implements RefundMoney{
 	@Override
 	public String refundDisplay(int id) {
 		OrderDetails o= repo.refundDisplay(id);
-		if(o.getStatus().equalsIgnoreCase("RETURNED"))	
+		
+		if(o.getStatus().equalsIgnoreCase("RETURNED"))	{
+			o.setStatus("REFUNDED");
+			repo.save(o);
 			return "Refund Successful";
-		else return "Sorry,Product is not Delivered You are not Eligible for Refund";
+		}	
+		else if(o.getStatus().equalsIgnoreCase("REFUNDED")) {
+			return "Refund done Already";
+		}
+		else if(o.getStatus().equalsIgnoreCase("Delivered")) {
+			return "Delivered Successfully";
+		}
+			
+		else return "Sorry,Product is not Delivered. You are not Eligible for Refund";
 
 	}
 
